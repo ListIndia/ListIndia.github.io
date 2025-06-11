9b3889c78c0a348803abb0b07aa278af500e5a44
ListIndia <215002183+ListIndia@users.noreply.github.com>
2025-06-11 12:51:46 +0530

    Make api call for recent data

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForOf, NgIf} from '@angular/common';
 import {SellerCardComponent} from '../seller-card/seller-card.component';
 import {Seller} from '../../shared/models/seller.entity';
 import {recentData} from '../../../../recent.data';
+import {DataService} from '../../data.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +13,18 @@ import {recentData} from '../../../../recent.data';
     NgIf,
     SellerCardComponent
   ],
+  providers: [DataService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  sellers: Seller[] = recentData;
+export class HomeComponent implements OnInit {
+  sellers: Seller[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    (this.dataService.getData() as Observable<{data: Seller[]}>).subscribe(res => {
+      this.sellers = res.data;
+    });
+  }
 }
