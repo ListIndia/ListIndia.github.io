ec841120dc77cfaa00b5f018ae90994d24d0ecf8
ListIndia <215002183+ListIndia@users.noreply.github.com>
2025-06-13 17:55:24 +0530

    scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from './components/header/header.component';
 import {Data} from './shared/data/data';
 import {Seller} from './shared/models/seller.entity';
-import {RouterOutlet} from '@angular/router';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {FooterComponent} from './components/footer/footer.component';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -18,4 +19,11 @@ import {FooterComponent} from './components/footer/footer.component';
 export class AppComponent {
   title = 'ListIndia';
   data: Seller[] = Data;
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        window.scrollTo({ top: 0 });
+      });
+  }
 }
